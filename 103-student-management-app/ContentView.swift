3a6b492ef57955c0a85eef46887e5b05aff0a9aa
Samantha Roman <samantharaeroman@gmail.com>
2025-10-01 15:16:12 -0400

    Initial Commit

---
+++ 103-student-management-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  103-student-management-app
+//
+//  Created by Samantha Roman on 10/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 103-student-management-app/_03_student_management_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  _03_student_management_appApp.swift
+//  103-student-management-app
+//
+//  Created by Samantha Roman on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct _03_student_management_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
